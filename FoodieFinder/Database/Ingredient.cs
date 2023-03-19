1426b55b7fc30d4018f6152bbe39fefa82c4f1e8
Ferales <Kuba.sadowski137@gmail.com>
2023-03-19 22:23:29 +0100

    initiall commit

---
+++ FoodieFinder/Database/Ingredient.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodieFinder.Database
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public string Unit { get; set; }
+        public int RecipeId { get; set; }
+
+    }
+
+    public class IngredientContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public IngredientContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("MyDatabase"));
+        }
+    }
+}
--- FoodieFinder/MauiProgram.cs
+++ FoodieFinder/MauiProgram.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using FoodieFinder.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Reflection;
 
 namespace FoodieFinder;
 
@@ -15,8 +19,24 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
+
+        var a = Assembly.GetExecutingAssembly();
+        using var stream = a.GetManifestResourceStream("FoodieFinder.appsettings.json");
+
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonStream(stream)
+            .Build();
+
+
+        builder.Configuration.AddConfiguration(configuration);
+
+        using (var context = new IngredientContext(configuration))
+        {
+            context.Database.Migrate();
+        }
+
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
 		return builder.Build();
