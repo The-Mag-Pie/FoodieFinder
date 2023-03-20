285bb7026d755c1f7b510d20400ab125b9120f96
The-Mag-Pie <barteksor2000@onet.pl>
2023-03-20 20:16:57 +0100

    db connection fixes

--- FoodieFinder/Database/AppDbContext.cs
+++ FoodieFinder/Database/AppDbContext.cs
@@ -1,27 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FoodieFinder.Database
 {
     public class AppDbContext : DbContext
     {
-        public DbSet<Ingredient> Ingredients { get; set; }
-        public DbSet<LikedRecipe> LikedRecipes { get; set; }
-        public DbSet<Recipe> Recipes { get; set; }
-        public DbSet<StoreRoom> StoreRoomItems { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<Ingredient> Ingredient { get; set; }
+        public DbSet<LikedRecipe> LikedRecipe { get; set; }
+        public DbSet<Recipe> Recipe { get; set; }
+        public DbSet<StoreRoom> StoreRoom { get; set; }
+        public DbSet<User> User { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-
-            Database.EnsureCreated();
-        }
     }
 }
