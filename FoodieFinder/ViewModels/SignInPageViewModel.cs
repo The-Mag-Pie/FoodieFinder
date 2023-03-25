cd7b94af7fe1f2c51de721bb9aa9082d5e717d04
Lukasz3388 <lukaszkolenda3388@gmail.com>
2023-03-25 18:43:29 +0100

    login and session

--- FoodieFinder/ViewModels/SignInPageViewModel.cs
+++ FoodieFinder/ViewModels/SignInPageViewModel.cs
@@ -16,6 +16,33 @@ namespace FoodieFinder.ViewModels
         private void SignIn()
         {
             //TODO: Logowanie
+            var log = new Login(_dbContext);
+            if(Email != null && Password != null) {
+                if(log.CheckIfInDatabase(Email, Password))
+                {
+                    //Application.Current.MainPage.DisplayAlert("Error", "Poprawne ok", "Ok");
+
+                    if (log.CreateSession(Email))
+                    {
+                        // SORKU TU PRZEJŚCIE DO GŁÓWNEJ STRONY TAK ZWANY HOME PAGE ok?
+                        Application.Current.MainPage.DisplayAlert("Error", log.GetUserNameSession(), "Ok");
+                    }
+                    else
+                    {
+                        Application.Current.MainPage.DisplayAlert("Error", "Session not created", "Ok");
+                    }
+
+                }
+                else
+                {
+                    Application.Current.MainPage.DisplayAlert("Error", "Incorrect Email or Password", "Ok");
+                }
+
+            }
+            else
+            {
+                Application.Current.MainPage.DisplayAlert("Error", "All boxes must be filled", "Ok");
+            }
         }
 
     }
