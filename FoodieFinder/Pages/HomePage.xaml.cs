bf614acffcb1986a349c79d587fef7cbf69e87ae
The-Mag-Pie <barteksor2000@onet.pl>
2023-03-27 00:53:42 +0200

    add ingredient to bucket list

--- FoodieFinder/Pages/HomePage.xaml.cs
+++ FoodieFinder/Pages/HomePage.xaml.cs
@@ -15,12 +15,22 @@ public partial class HomePage : ContentPage
 		vm = new(appDbContext, userData);
 		BindingContext = vm;
 
-		Disappearing += (s, e) => vm.OnDisappearing();
+		Disappearing += (s, e) => vm.SaveBucketList();
 	}
 
     protected override bool OnBackButtonPressed()
     {
-		vm.OnDisappearing();
+		vm.SaveBucketList();
         return base.OnBackButtonPressed();
     }
+
+    private void AddIngredientTapped(object sender, EventArgs e)
+    {
+		// Hide soft keyboard after add button clicked
+		//addIngredientEntry.Unfocus();
+
+		// Workaround for MAUI bug - soft keyboard is not hiding when entry is unfocused
+		addIngredientEntry.IsEnabled = false;
+		addIngredientEntry.IsEnabled = true;
+	}
 }
