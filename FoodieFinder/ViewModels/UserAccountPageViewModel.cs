bc6ac7bbc35cf24bb32f9f6e95688d55aa2e40ff
Ferales <Kuba.sadowski137@gmail.com>
2023-04-23 15:56:09 +0200

    added interface for changing password and email

---
+++ FoodieFinder/ViewModels/UserAccountPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FoodieFinder.Database;
+using FoodieFinder.LocalJsonDatabase;
+using FoodieFinder.Popups;
+using FoodieFinder.UserAccount;
+
+namespace FoodieFinder.ViewModels
+{
+    partial class UserAccountPageViewModel : BaseViewModel
+    {
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(UserFirstLetter))]
+        private string _welcomeUser;
+        private readonly IServiceProvider _serviceProvider;
+        public string UserFirstLetter => WelcomeUser.First().ToString().ToUpper();
+        [ObservableProperty]
+        private string oldPassword;
+        [ObservableProperty]
+        private string newPassword;
+        [ObservableProperty]
+        private string confirmedPassword;
+        [ObservableProperty]
+        private string newEmail;
+        [ObservableProperty]
+        private string confirmedEmail;
+        public UserAccountPageViewModel(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+
+            var dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
+            var userData = _serviceProvider.GetRequiredService<UserData>();
+
+            var username = userData.UserName;
+            var atIdx = username.LastIndexOf('@');
+            if (atIdx > -1)
+            {
+                WelcomeUser = username[..atIdx];
+            }
+            else
+            {
+                WelcomeUser = username;
+            }
+
+        }
+    }
+}
