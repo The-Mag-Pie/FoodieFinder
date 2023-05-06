75a7cc1aade131e6b0c26dc1bbe75b5505fb4322
The-Mag-Pie <barteksor2000@onet.pl>
2023-05-06 14:52:47 +0200

    reload saved recipes page when page appears

--- FoodieFinder/Pages/SavedRecipePage.xaml.cs
+++ FoodieFinder/Pages/SavedRecipePage.xaml.cs
@@ -4,10 +4,14 @@ namespace FoodieFinder.Pages;
 
 public partial class SavedRecipePage : ContentPage
 {
+    private readonly SavedRecipePageViewModel vm;
+
     public SavedRecipePage(IServiceProvider serviceProvider)
 	{
 		InitializeComponent();
-        BindingContext = new SavedRecipePageViewModel(serviceProvider);
+        vm = new SavedRecipePageViewModel(serviceProvider);
+        BindingContext = vm;
 
+        Appearing += (s, e) => vm.LoadSavedItems();
     }
 }
