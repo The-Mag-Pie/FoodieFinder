6b995e95755a61390466f6ec9242b49f5889b977
Lukasz3388 <lukaszkolenda3388@gmail.com>
2023-05-14 15:08:26 +0200

    SearchPageUpdate

--- FoodieFinder/ViewModels/SearchPageViewModel.cs
+++ FoodieFinder/ViewModels/SearchPageViewModel.cs
@@ -11,9 +11,12 @@ namespace FoodieFinder.ViewModels
     partial class SearchPageViewModel : BaseViewModel
     {
 
-
+        public ObservableCollection<OnlineRecipe> SearchedRecipes { get; } = new();
+        public ObservableCollection<OnlineRecipe> RecentSearch { get; } = new();
         [ObservableProperty]
         private bool _isYourRecipesVisible = false;
+        [ObservableProperty]
+        private string searched;
 
         [ObservableProperty]
         private string _addIngredientName = string.Empty;
@@ -28,10 +31,38 @@ namespace FoodieFinder.ViewModels
 
             _dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
             _userData = _serviceProvider.GetRequiredService<UserData>();
-
+            OnlineRecipe item = new OnlineRecipe();
+            item.Id = 1;
+            item.RecipeName = "Testowa";
+            item.UserId = 1;
+            SearchedRecipes.Add(item);
+            SearchedRecipes.Add(item);
+            SearchedRecipes.Add(item);
+            RecentSearch.Add(item);
+            RecentSearch.Add(item);
+            RecentSearch.Add(item);
 
         }
-
+        private void SearchItemTapped(OnlineRecipe ORecipe)
+        {
+            //----------------------------------
+        }
+        private void RecentSearchTapped(OnlineRecipe RecentSearchRecipe)
+        {
+            //----------------------------------
+        }
+        private void DeleteRecentSearchTapped(OnlineRecipe RecentSearchRecipe)
+        {
+            //----------------------------------
+        }
+        private void Search(OnlineRecipe RecentSearchRecipe)
+        {
+            //----------------------------------
+        }
+        private void FiltersTapped()
+        {
+            //----------------------------------
+        }
 
 
 
