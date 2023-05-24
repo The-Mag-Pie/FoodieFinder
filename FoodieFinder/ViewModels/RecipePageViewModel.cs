bac0de6e1ecc3104d5faea4226033e950833d76f
Ferales <Kuba.sadowski137@gmail.com>
2023-05-24 18:35:31 +0200

    implemented checkboxes logic on recipe page

--- FoodieFinder/ViewModels/RecipePageViewModel.cs
+++ FoodieFinder/ViewModels/RecipePageViewModel.cs
@@ -4,6 +4,7 @@ using FoodieFinder.Database;
 using FoodieFinder.Models;
 using Microsoft.Extensions.Configuration;
 using System.Collections.ObjectModel;
+using FoodieFinder.UserAccount;
 
 namespace FoodieFinder.ViewModels
 {
@@ -11,7 +12,8 @@ namespace FoodieFinder.ViewModels
     {
         public ObservableCollection<IngredientModel> Ingredients { get; } = new();
         [ObservableProperty]
-        public Recipe tappedRecipe;
+        private Recipe tappedRecipe;
+        private static List<StorageItem> StorageItems = new();
         private readonly IServiceProvider _serviceProvider;
 
         public RecipePageViewModel(IServiceProvider serviceProvider, Recipe recipe)
@@ -20,7 +22,13 @@ namespace FoodieFinder.ViewModels
             tappedRecipe = recipe;
 
             var dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
-            Random xd = new Random();
+            var userData = _serviceProvider.GetRequiredService <UserData>();
+
+            StorageItems.Clear();
+            foreach (var item in dbContext.StoreRoom.Where(u => u.User_UserId == userData.UserId))
+            {
+                StorageItems.Add(item);
+            }
 
             foreach (var ingredient in dbContext.Ingredient.Where(i => i.RecipeId == recipe.Id))
             {
@@ -29,7 +37,7 @@ namespace FoodieFinder.ViewModels
                     IngredientName = ingredient.Name,
                     Unit = ingredient.Unit,
                     Quantity = ingredient.Quantity,
-                    IsChecked = Convert.ToBoolean(xd.Next(0, 2))
+                    IsChecked = IsIngredientInStorage(ingredient)
                 });
             }
         }
@@ -56,6 +64,23 @@ namespace FoodieFinder.ViewModels
         private void Back()
         {
             Shell.Current.SendBackButtonPressed();
+        }
+
+        private static bool IsIngredientInStorage(Ingredient ingredient)
+        {
+            var matchedIngredient = StorageItems.Where(i => String.Equals(i.ProductName, ingredient.Name, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            if(matchedIngredient != null)
+            {
+                if(matchedIngredient.Unit == ingredient.Unit)
+                {
+                    return matchedIngredient.Quantity >= ingredient.Quantity;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
