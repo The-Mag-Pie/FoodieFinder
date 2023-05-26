5d52a7ba94d1003a73f97198ec7ebe4d0aa56666
The-Mag-Pie <barteksor2000@onet.pl>
2023-05-27 00:47:56 +0200

    adapt recipe share functionality to online recipe share

--- FoodieFinder/AppShellUser.xaml.cs
+++ FoodieFinder/AppShellUser.xaml.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Maui.Core;
 using CommunityToolkit.Maui.Views;
 using FoodieFinder.Database;
 using FoodieFinder.Popups;
+using FoodieFinder.SuggesticAPI;
 using FoodieFinder.UserAccount;
 
 namespace FoodieFinder;
@@ -24,48 +25,100 @@ public partial class AppShellUser : Shell
 	private async Task OnSharedRecipeIntentReceived()
 	{
 		var recipeID = App.SharedRecipeID;
-		if (recipeID != null)
-		{
-			var dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
-            
-            Recipe recipe;
-			try
-			{
-                recipe = dbContext.Recipe.Where(r => r.Id == recipeID).Single();
-            }
-			catch
-			{
-				await DisplayAlert("Shared recipe error", "Shared recipe not found.", "OK");
-                App.SharedRecipeID = null;
-				return;
-            }
+		if (recipeID is not null)
+            await HandleSharedRecipe(recipeID);
 
-            var userData = _serviceProvider.GetRequiredService<UserData>();
-			if (recipe.UserId == userData.UserId)
-			{
-                await DisplayAlert("Shared recipe error", "You can't save your own recipe.", "OK");
-                App.SharedRecipeID = null;
-				return;
-            }
+        var onlineRecipeID = App.SharedOnlineRecipeID;
+        if (onlineRecipeID is not null)
+            await HandleSharedOnlineRecipe(onlineRecipeID);
+    }
 
-            var popup = new SaveSharedRecipePopup(recipe.Name);
-			var result = await this.ShowPopupAsync(popup);
+	private async Task HandleSharedRecipe(int? recipeID)
+	{
+        var dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
 
-			if (result is true)
-			{
-				
-				dbContext.Recipe.Add(new Recipe()
-				{
-					Name = recipe.Name,
-					Description = recipe.Description,
-					Preparation = recipe.Preparation,
-					UserId = userData.UserId
-				});
-				dbContext.SaveChanges();
-                await Toast.Make("Recipe saved", ToastDuration.Long).Show();
-            }
-
-			App.SharedRecipeID = null;
+        Recipe recipe;
+        try
+        {
+            recipe = dbContext.Recipe.Where(r => r.Id == recipeID).Single();
         }
-	}
+        catch
+        {
+            await DisplayAlert("Shared recipe error", "Shared recipe not found.", "OK");
+            App.SharedRecipeID = null;
+            return;
+        }
+
+        var userData = _serviceProvider.GetRequiredService<UserData>();
+        if (recipe.UserId == userData.UserId)
+        {
+            await DisplayAlert("Shared recipe error", "You can't save your own recipe.", "OK");
+            App.SharedRecipeID = null;
+            return;
+        }
+
+        var popup = new SaveSharedRecipePopup(recipe.Name);
+        var result = await this.ShowPopupAsync(popup);
+
+        if (result is true)
+        {
+
+            dbContext.Recipe.Add(new Recipe()
+            {
+                Name = recipe.Name,
+                Description = recipe.Description,
+                Preparation = recipe.Preparation,
+                UserId = userData.UserId
+            });
+            dbContext.SaveChanges();
+            await Toast.Make("Recipe saved", ToastDuration.Long).Show();
+        }
+
+        App.SharedRecipeID = null;
+    }
+
+    private async Task HandleSharedOnlineRecipe(string recipeID)
+    {
+        var dbContext = _serviceProvider.GetRequiredService<AppDbContext>();
+        var apiClient = _serviceProvider.GetRequiredService<SuggesticApiClient>();
+        var userData = _serviceProvider.GetRequiredService<UserData>();
+
+        var onlineRecipe = await apiClient.GetRecipeById(recipeID);
+
+        if (onlineRecipe is null)
+        {
+            await DisplayAlert("Shared recipe error", "Shared recipe not found.", "OK");
+            App.SharedOnlineRecipeID = null;
+            return;
+        }
+
+        var isOnlineRecipeSavedByUser = dbContext.OnlineRecipe
+            .Where(r => r.UserId == userData.UserId)
+            .Any(r => r.RecipeApiId == recipeID);
+
+        if (isOnlineRecipeSavedByUser)
+        {
+            await DisplayAlert("Shared recipe error", "This recipe has been previously saved by you.", "OK");
+            App.SharedOnlineRecipeID = null;
+            return;
+        }
+
+        var popup = new SaveSharedRecipePopup(onlineRecipe.Name);
+        var result = await this.ShowPopupAsync(popup);
+
+        if (result is true)
+        {
+            dbContext.OnlineRecipe.Add(new()
+            {
+                RecipeApiId = recipeID,
+                RecipeName = onlineRecipe.Name,
+                UserId = userData.UserId
+            });
+
+            dbContext.SaveChanges();
+            await Toast.Make("Recipe saved", ToastDuration.Long).Show();
+        }
+
+        App.SharedOnlineRecipeID = null;
+    }
 }
