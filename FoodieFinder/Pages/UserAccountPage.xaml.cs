4537316222a69055ea1160dc652db90280ff48e1
Ferales <Kuba.sadowski137@gmail.com>
2023-06-07 21:39:02 +0200

    fix(PZSI-14) changed font color to more visible

--- FoodieFinder/Pages/UserAccountPage.xaml.cs
+++ FoodieFinder/Pages/UserAccountPage.xaml.cs
@@ -1,4 +1,5 @@
 using FoodieFinder.ViewModels;
+using Microsoft.Maui.Controls;
 
 namespace FoodieFinder.Pages;
 
@@ -11,5 +12,32 @@ public partial class UserAccountPage : ContentPage
 
         vm = new(serviceProvider);
         BindingContext = vm;
+
+        //oldPassword.Focused += OnEntryFocused;
+        //oldPassword.Unfocused += OnEntryUnfocused;
+
+        //newPassword.Focused += OnEntryFocused;
+        //newPassword.Unfocused += OnEntryUnfocused;
+
+        //confirmedPassword.Focused += OnEntryFocused;
+        //confirmedPassword.Unfocused += OnEntryUnfocused;
+
+        //newEmail.Focused += OnEntryFocused;
+        //newEmail.Unfocused += OnEntryUnfocused;
+
+        //confirmedEmail.Focused += OnEntryFocused;
+        //confirmedEmail.Unfocused += OnEntryUnfocused;
+
     }
+
+    //private void OnEntryFocused(object sender, FocusEventArgs e)
+    //{
+    //    userPicture.IsVisible = false;
+        
+    //}
+
+    //private void OnEntryUnfocused(object sender, FocusEventArgs e)
+    //{
+    //    userPicture.IsVisible = true;
+    //}
 }
